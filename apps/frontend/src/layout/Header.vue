86ec372dafb1f066b3251a85b5d5d6a68e691f3f
Zsolt Tasnadi <rastasi@gmail.com>
2025-05-06 12:33:44 +0200

    header with sublinks + temporary logo

--- apps/frontend/src/layout/Header.vue
+++ apps/frontend/src/layout/Header.vue
@@ -1,29 +1,54 @@
 <template>
-    <v-app-bar flat>
-        <v-container class="mx-auto d-flex align-center justify-center">
-        <v-avatar class="me-4 " color="grey-darken-1" size="32"></v-avatar>
+  <v-app-bar flat>
+    <v-container class="mx-auto d-flex align-center justify-center">
+      <v-img :src="logo" alt="LibrePM" height="70" />
 
-        <v-btn v-for="link in links" :key="link.key" :text="link.text" :to="link.to" variant="text"></v-btn>
+      <template v-for="link in links" :key="link.key">
+        <v-btn :text="link.text" :to="link.to" variant="text" v-if="!link.sublinks"></v-btn>
+        <template v-else>
+          <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" variant="text">{{ link.text }}</v-btn>
+            </template>
 
-        <v-spacer></v-spacer>
+            <v-list>
+              <v-list-item v-for="(sublink, index) in link.sublinks" :key="index" :value="index" :to="sublink.to">
+                <v-list-item-title>{{ sublink.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+      </template>
+      <v-spacer></v-spacer>
 
-        <v-responsive max-width="160">
-            <v-text-field density="compact" :label="t('global.search')" rounded="lg" variant="solo-filled" flat hide-details
-            single-line></v-text-field>
-        </v-responsive>
-        </v-container>
-    </v-app-bar>
+      <v-responsive max-width="160">
+        <v-text-field density="compact" :label="t('global.search')" rounded="lg" variant="solo-filled" flat hide-details
+          single-line></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-import { useI18n } from 'vue-i18n'; 
+import { useI18n } from 'vue-i18n';
+import logo from '@/assets/logo.png'
+
 const { t } = useI18n();
 
-interface Link {
+interface Sublink {
   text: string;
   key: string;
   to: string;
 };
+
+
+interface Link {
+  text: string;
+  key?: string;
+  to?: string;
+  sublinks?: Sublink[];
+};
+
 
 const links: Link[] = [
   {
@@ -52,15 +77,21 @@ const links: Link[] = [
     to: '/filter'
   },
   {
-    text: 'Statuses',
-    key: 'statuses',
-    to: '/status'
+    text: 'Administration',
+    sublinks: [
+      {
+        text: 'Statuses',
+        key: 'statuses',
+        to: '/status'
+      },
+      {
+        text: 'Trackers',
+        key: 'trackers',
+        to: '/tracker'
+      },
+    ],
   },
-  {
-    text: 'Trackers',
-    key: 'trackers',
-    to: '/tracker'
-  },
+
   /*  {
       text: 'Profile',
       key: 'profile',
