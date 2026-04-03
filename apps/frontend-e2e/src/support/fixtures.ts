3f16854b0a52c348c990403810f3d4801eae5da4
Zsolt Tasnadi <rastasi@gmail.com>
2026-04-03 17:26:19 +0200

    e2e to ci

--- apps/frontend-e2e/src/support/fixtures.ts
+++ apps/frontend-e2e/src/support/fixtures.ts
@@ -1,4 +1,4 @@
-import { test as base, Page } from '@playwright/test';
+import { test as base, expect, Page } from '@playwright/test';
 
 type Fixtures = {
   authenticatedPage: Page;
@@ -10,7 +10,7 @@ export const test = base.extend<Fixtures>({
     await page.getByLabel('E-mail').fill('testuser@example.com');
     await page.getByLabel('Password').fill('password123');
     await page.getByRole('button', { name: 'Login' }).click();
-    await page.waitForURL(/\/dashboard/);
+    await expect(page).toHaveURL(/\/dashboard/, { timeout: 30000 });
     await use(page);
   },
 });
